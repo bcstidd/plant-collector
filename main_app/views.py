2f59dcfb23fc4c7fa90429834250623346fe9627
bcstidd <bc.stidd@gmail.com>
2023-02-06 21:35:37 -0800

    Adds create functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView 
 from .models import Plant
 # Create your views here.
-plants = [
-    {'name': 'Nerve plant', 'category': 'Indoor/tropical', 'thrivesIn':'Bright/indirect light', 'difficulty':'Easy'},
-    {'name': 'Pothos', 'category': 'Climber', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Easy'},
-    {'name': 'Snake Plant', 'category': 'Succulent', 'thrivesIn': 'Full sun || Low light', 'difficulty': 'Easy'},
-    {'name': 'Giant Chain Fern', 'category': 'Shrub', 'thrivesIn': 'Indirect light', 'difficulty': 'Moderate-Hard'},
-    {'name': 'Monstera deliciosa', 'category': 'Tropical Shrub/Vine', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Moderate'},
-    {'name': 'String of Hearts', 'category': 'Tropical Vine', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Moderate'},
-
-    ]
 
 def home(request):
     return render(request, 'home.html')
@@ -26,3 +18,8 @@ def plants_index(request):
 def plants_detail(request, plant_id):
     plant = Plant.objects.get(id=plant_id)
     return render(request, 'plants/detail.html', {'plant': plant})
+
+class PlantCreate(CreateView):
+    model = Plant
+    fields = '__all__'
+    success_url = '/plants'
