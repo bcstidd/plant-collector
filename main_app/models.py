ea9f508a15612c7fb5e83cc70ca116918a1dba5f
bcstidd <bc.stidd@gmail.com>
2023-02-06 20:55:34 -0800

    Added Details page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    thrivesIn = models.CharField(max_length=250)
+    difficulty = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
+from .models import Plant
 # Create your views here.
 plants = [
-    {'name': 'Nerve plant', 'plantclass': 'Indoor/tropical', 'thrivesin':'Bright/indirect light', 'difficulty':'Easy'},
-    {'name': 'Pothos', 'plantclass': 'Climber', 'thrivesin': 'Bright/indirect light', 'difficulty': 'Easy'},
-    {'name': 'Snake Plant', 'plantclass': 'Succulent', 'thrivesin': 'Full sun || Low light', 'difficulty': 'Easy'},
-    {'name': 'Giant Chain Fern', 'plantclass': 'Shrub', 'thrivesin': 'Indirect light', 'difficulty': 'Moderate-Hard'},
-    {'name': 'Monstera deliciosa', 'plantclass': 'Tropical Shrub/Vine', 'thrivesin': 'Bright/indirect light', 'difficulty': 'Moderate'},
-    {'name': 'String of Hearts', 'plantclass': 'Tropical Vine', 'thrivesin': 'Bright/indirect light', 'difficulty': 'Moderate'},
+    {'name': 'Nerve plant', 'category': 'Indoor/tropical', 'thrivesIn':'Bright/indirect light', 'difficulty':'Easy'},
+    {'name': 'Pothos', 'category': 'Climber', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Easy'},
+    {'name': 'Snake Plant', 'category': 'Succulent', 'thrivesIn': 'Full sun || Low light', 'difficulty': 'Easy'},
+    {'name': 'Giant Chain Fern', 'category': 'Shrub', 'thrivesIn': 'Indirect light', 'difficulty': 'Moderate-Hard'},
+    {'name': 'Monstera deliciosa', 'category': 'Tropical Shrub/Vine', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Moderate'},
+    {'name': 'String of Hearts', 'category': 'Tropical Vine', 'thrivesIn': 'Bright/indirect light', 'difficulty': 'Moderate'},
 
     ]
 
@@ -18,6 +18,11 @@ def about(request):
     return render(request, 'about.html')
 
 def plants_index(request):
+    plants = Plant.objects.all()
     return render(request, 'plants/index.html', {
         'plants': plants,
     })
+
+def plants_detail(request, plant_id):
+    plant = Plant.objects.get(id=plant_id)
+    return render(request, 'plants/detail.html', {'plant': plant})
