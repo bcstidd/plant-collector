0f987c3575a45b5f47244b25df8e5acf5474684a
bcstidd <bc.stidd@gmail.com>
2023-02-06 21:52:59 -0800

    Finished CRUD , Lab 3 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from .models import Plant
 # Create your views here.
 
@@ -22,4 +22,12 @@ def plants_detail(request, plant_id):
 class PlantCreate(CreateView):
     model = Plant
     fields = '__all__'
+    success_url = '/plants'
+
+class PlantUpdate(UpdateView):
+    model = Plant
+    fields = ['category', 'difficulty']
+
+class PlantDelete(DeleteView):
+    model = Plant
     success_url = '/plants'
